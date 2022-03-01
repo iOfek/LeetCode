2ec91a2996fe8e176af8a946a880c80ba9b0aa7c
iOfek <iofek.s@gmail.com>
2022-03-01 02:38:40 +0200

    Day 1

--- ContainerWithMostWater.py
+++ ContainerWithMostWater.py
@@ -1,12 +1,28 @@
 class Solution(object):
+    
+    
+    
     def maxArea(self, height):
         """
         :type height: List[int]
         :rtype: int
         """
-        
+        max_container = 0
+        l = 0
+        r = len(height) -1        
+        while l < r:
+            width = abs(l-r)
+            area = min(height[l],height[r]) * width                   
+            max_container =max(max_container,area)
+            if (height[l] <= height[r]):
+                l+=1
+            else :r-=1
+         
+        return max_container
+    
+            
 def main():
-    height =[]
+    height = [2,3,4,5,18,17,6]
     object = Solution()
     print(object.maxArea(height))
 
