3eae7f4d1d6f6ce380cf73ae1bb363974596a597
iOfek <iofek.s@gmail.com>
2022-03-07 00:49:20 +0200

    final

--- SodukoSolver.py
+++ SodukoSolver.py
@@ -1,6 +1,7 @@
 
 
 from math import floor
+import re
 
 
 class Solution(object):
@@ -13,60 +14,65 @@ class Solution(object):
         :rtype: None Do not return anything, modify board in-place instead.
         """
         
-        def isValidSudoku(board):
+        rows = []
+        columns = []
+        boxes = [[[], [], []], [[], [], []], [[], [], []]]
+        
+        rowsEmptyIndexes =[]
+        colsEmptyIndexes =[]
+        boxesEmptyIndexes =[[[], [], []], [[], [], []], [[], [], []]]
+        for i in range(9):
+            row = []
+            rowEmptyIndexes =[]
+            for j in range(9):
+                if (board[i][j] != "."):
+                    row.append(board[i][j])
+                else:
+                    rowEmptyIndexes.append((i,j)) 
+            rows.append(row)
+            rowsEmptyIndexes.append(rowEmptyIndexes)
+        for j in range(9):
+            column = []
+            colEmptyIndexes =[]
+            for i in range(9):
+                if (board[i][j] != "."):
+                    column.append(board[i][j])
+                else:
+                    colEmptyIndexes.append((i,j)) 
+            columns.append(column)
+            colsEmptyIndexes.append(colEmptyIndexes)
+        for i in range(9):
+            for j in range(9):
+                if (board[i][j] != "."):
+                    boxes[i // 3][j // 3].append(board[i][j])
+                else:
+                    boxesEmptyIndexes[i // 3][j // 3].append((i,j))
+        
+        
+        def isValid(row,col,value):
             """
             :type board: List[List[str]]
             :rtype: bool
             """
             
+            def checkRow(row,value):
+                if value in rows[row]:
+                    return False
+                return True
             
-            def checkRow(i,j,value, map):
-                rowIndex= 'row'+str(i+1)
-                row = map.get(rowIndex)
-                if value in row:
+            def checkCol(col,value):
+                if value in columns[col]:
                     return False
-                row.append(value)
-                return True  
-            
-            def checkCol(i,j,value, map):
-                colIndex= 'col'+str(j+1)
-                col = map.get(colIndex)
-                if value in col:
+                return True 
+                
+            def checkBox(row,col,value):
+                if value in boxes[row // 3][col // 3]:
                     return False
-                col.append(value)
-                return True  
+                return True
                 
-            def checkBox(i,j,value, map):
-                row = floor(i/3) +1
-                col = floor(j/3) +1
                 
-                box = col
-                if(row == 2): box +=3
-                elif row == 3: box+=6
-                box = int(box)
-                boxIndex= 'box'+str(box)
-                mapBox = map.get(boxIndex)
-                if value in mapBox:
-                    return False
-                mapBox.append(value)
-                return True  
-                
-            map = {}
-            for i in range(1,10):
-                map['row'+str(i)] = []
-                map['col'+str(i)] = []
-                map['box'+str(i)] = []
-
-            for i in range(9):
-                for j in range(9):
-                    value = board[i][j] 
-                    if value == ".": 
-                        continue
-                    value = int(value)
-                    if  value > 9 or value < 1:
-                        return False
-                    if not checkRow(i,j,value,map) or not checkCol(i,j,value,map)  or not checkBox(i,j,value,map):
-                        return False 
+            if not checkRow(row,value) or not checkCol(col,value)  or not checkBox(row,col,value):
+                return False 
             return True
 
         def findNextCelltoFill(board):
@@ -76,113 +82,95 @@ class Solution(object):
                         return i , j
             return -1 , -1    
         
-        def getBox(boxNumber):
-            boxIndexes=[]
-            colAdd=0
-            rowAdd=0
-            if(boxNumber in {1,4,7}):
-                colAdd = 3
-            if(boxNumber in {2,5,8}):
-                colAdd = 6    
-            if(boxNumber in {3,4,5}):
-                rowAdd = 3
-            if(boxNumber in {6,7,8}):
-                rowAdd = 6
-            for i in range (3):
-                for j in range (3):                    
-                    boxIndexes.append((i+rowAdd,j+colAdd))
-            return boxIndexes
         
-        def makeImplications(board):
+        def makeImplications(board,i,j,val):
+            board [i][j] =val
+            rows[i].append(val)
+            columns[j].append(val)
+            boxes[i//3][j//3].append(val)
+            boxesEmptyIndexes[i//3][j//3].remove((i,j))
             impl = []
+            impl.append((i,j,val))
+            i=0
+            j=0
             changed = True
             # while changes are available
             while changed:
                 changed = False
                 # for each box
-                for i in range(9):
-                    # get box Indexes
-                    box = getBox(i)
+                for i in range(3):
+                    for j in range(3):
+            #             # initialize box values
+                        missingBoxValues = self.complete.copy()
+            #             # find missing box values 
+                        # boxValues = set(boxes[i][j])
+                        missingBoxValues = missingBoxValues.difference(set(boxes[i][j]))
                     
-                    # initialize box values
-                    missingBoxValues = self.complete.copy()
-                    # contains info for each index in box
-                    boxInfo = []
-                    # find missing box values and 
-                    for index in box:
-                        i,j = index
-                        value = board [i][j]
-                        if value != ".":
-                            missingBoxValues.remove(value)
-                    # save copy missing values for each empy index in box
-                    for index in box:
-                        i,j = index
-                        value = board [i][j]
-                        if value == ".":
-                            boxInfo.append((i,j,missingBoxValues.copy()))
-                    
-                    for element in boxInfo:
-                        left = set()
-                        # get row numbers
-                        row = element[0]
-                        rowValues = set()
-                        for value in  board[row]:
-                            if value != '.':
-                                rowValues.add(value)
-                        left = element[2].difference(rowValues)
-                        
-                        # get col numbers
-                        col = element[1]
-                        colValues = set()
-                        for i in  range(9):
-                            value = board[i][col]
-                            if value != '.':
-                                colValues.add(value)
-                        left = element[2].difference(colValues)
+            #             # save copy missing values for each empy index in box
+                        for index in boxesEmptyIndexes[i][j]:
+                            r,c = index
+                            rowValuesSet = set(rows[r])
+                            left = missingBoxValues.difference(rowValuesSet)
+                            colValuesSet = set(columns[c])
+                            left = left.difference(colValuesSet)
                                                 
-                        # check if left is singleton
-                        if len(left)== 1:
-                            val = left.pop()
-                            board[row][col] = str(val)
-                            if isValidSudoku(board):
-                                impl.append((row,col,val))
-                                changed = True
-                            else:
-                                board[row][col] = "."
+                            # check if left is singleton
+                            if len(left)== 1:
+                                val = str(left.pop())
+                                if isValid(r,c,val):
+                                    board[r][c] = val
+                                    impl.append((r,c,val))
+                                    rows[r].append(val)
+                                    columns[c].append(val)
+                                    boxes[r//3][c//3].append(val)
+                                    boxesEmptyIndexes[r//3][c//3].remove((r,c))
+                                    changed = True
+                            
                                         
             return impl
         
         def undoImplications(board, impl):
             for element in impl:
+                val =element[2]
                 i,j=element[0],element[1]
                 board[i][j] = "."
+                rows[i].remove(val)
+                columns[j].remove(val)
+                boxes[i//3][j//3].remove(val)
+                boxesEmptyIndexes[i//3][j//3].append((i,j))
             return
         
         def solve(board,i,j):
             i,j = findNextCelltoFill(board)
-            
             if i == -1:
                 return True
             
             for value in range(1,10):
-                board[i][j] = str(value)
+                val = str(value)
                 # impl = []
-                if isValidSudoku(board):
-                    impl = makeImplications(board)
+                if i == 0 and j ==2 and val == '4':
+                    print()
+                if isValid(i,j,val):
                     
+                    impl = makeImplications(board,i,j,val)
                     if solve(board,i,j):
                         return True
-                # undo
+                    # undo
                     self.backtracks+=1
                     undoImplications(board,impl)
-                board[i][j] = "."
-            
+                    
+                    
             return False
-            
+        
+        
         solve(board,0,0)
         return self.backtracks
+
 def main():
-    board = [[".",".","9","7","4","8",".",".","."],["7",".",".",".",".",".",".",".","."],[".","2",".","1",".","9",".",".","."],[".",".","7",".",".",".","2","4","."],[".","6","4",".","1",".","5","9","."],[".","9","8",".",".",".","3",".","."],[".",".",".","8",".","3",".","2","."],[".",".",".",".",".",".",".",".","6"],[".",".",".","2","7","5","9",".","."]]
+    board = [["5","3",".",".","7",".",".",".","."],
+             ["6",".",".","1","9","5",".",".","."],
+             [".","9","8",".",".",".",".","6","."],
+             ["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
     object = Solution()
     
     print (object.solveSudoku(board))
